d08c716d894518cf245d53186e6ad7fda7327f0b
235913 <kacper.devek@gmail.com>
2022-04-04 14:02:53 +0100

    1.4 - Stale i annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int NUMBERS_SIZE = 12;
+    public static final int MAX_STACK_SIZE = 11;
+    public static final int EMPTY = -1;
+    public static final int INITIAL_VALUE = -1;
+    final private int[] numbers = new int[NUMBERS_SIZE];
 
-    public int total = -1;
+    public int total = INITIAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_STACK_SIZE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return numbers[total--];
     }
 
