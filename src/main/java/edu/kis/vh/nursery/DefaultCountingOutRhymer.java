64ce2bbcd02a8a88f8797d8376891801e93292c3
KacperZielinski <236021@edu.p.lodz.pl>
2022-04-25 13:59:17 +0100

    Constant and annotation changes added

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private final int[] numbers = new int[12];
+	public static final int TOTAL = -1;
+	public static final int SIZE = 12;
+	public static final int FULL = 11;
 
-	public int total = -1;
+	private final int[] numbers = new int[SIZE];
+
+	public int total = TOTAL;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,11 +16,11 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == TOTAL;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == FULL;
 	}
 
 	protected int peekaboo() {
