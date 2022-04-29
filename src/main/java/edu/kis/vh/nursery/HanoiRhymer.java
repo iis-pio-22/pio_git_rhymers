1b0b7ea4d007011b9a614d8d8968f50a353573cb
Maciej <mojkmac@gmail.com>
2022-04-29 10:43:54 +0200

    Błędy formatowania 1

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,19 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
+// Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane?
+// blędy wcięć w liniach 11-16 - ciało metody countIn()
+// brak wcięcia dla atrybutuw linii 5
