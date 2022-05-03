b31bd681202d2ab679ec6a26d0b3b2d4dab6dfb2
juliabalcer <235824@edu.p.lodz.pl>
2022-05-03 19:02:13 +0200

    dodanie final i poprawienie wartosci inicjalizujacych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,13 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	public static final int EMPTY_VALUE = -5;
-	private static final int NUMBERS_SIZE = 12;
+	public static final int EMPTY_VALUE = -1;
+	private static final int NUMBERS_MAX_SIZE = 12;
 	private static final int EMPTY_POSITION = -1;
-	private static final int FULL_POSITION = NUMBERS_SIZE - 1;
+	private static final int FULL_POSITION = NUMBERS_MAX_SIZE - 1;
 
 
-	private int[] numbers = new int[NUMBERS_SIZE];
+	private final int[] numbers = new int[NUMBERS_MAX_SIZE];
 
 	public int total = EMPTY_POSITION;
 
