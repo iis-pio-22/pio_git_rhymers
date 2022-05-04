569c66e77c302644db56dc04a3713981bc3a01f6
Arkinefed <arkinefedmail@gmail.com>
2022-05-04 09:09:28 +0200

    1.5.6 wygenerowano dokumentację dla klasy Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * @author Arkinefed
+ *
+ */
 public class Node {
 
 
@@ -8,26 +12,44 @@ public class Node {
 	private Node next;
 
 
+	/**
+	 * @param i - wartość, która ma być przechowywana przez węzeł
+	 */
 	public Node(int i) {
 		value = i;
 	}
 
+	/**
+	 * @return wartość przechowywaną przez węzeł
+	 */
 	public int getValue() {
 		return value;
 	}
 
+	/**
+	 * @return poprzedni węzeł
+	 */
 	public Node getPrev() {
 		return prev;
 	}
 
+	/**
+	 * @param prev - węzeł, który ma być ustawiony, jako poprzedni
+	 */
 	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
 
+	/**
+	 * @return nastęny węzeł
+	 */
 	public Node getNext() {
 		return next;
 	}
 
+	/**
+	 * @param next - węzeł, który ma być ustawiony, jako następny
+	 */
 	public void setNext(Node next) {
 		this.next = next;
 	}
