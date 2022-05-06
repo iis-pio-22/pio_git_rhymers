164a87a757a2fa4b912b37fcc89bfa98480b508d
wojtekczajka <w.czajka00@gmail.com>
2022-05-06 17:10:28 +0200

    accessory and encapsulation added.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	public static final int TOTAL = -1;
-	public static final int numberSize = 12;
+	private static final int TOTAL = -1;
+	private static final int numberSize = 12;
 	private final int[] numbers = new int[numberSize];
+	private int total = TOTAL;
 
-	public int total = TOTAL;
+	public int getTotal() {
+		return total;
+	}
 
 	public void countIn(int in) {
 		if (!isFull())
