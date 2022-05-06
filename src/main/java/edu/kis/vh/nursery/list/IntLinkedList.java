5ed701bedbf9a56613d5dc4ba0a2862951e93b9b
MaciejIwan <64867638+MaciejIwan@users.noreply.github.com>
2022-05-06 23:34:21 +0200

    added comment for docs

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -5,6 +5,10 @@ public class IntLinkedList {
     private Node last;
     private int value;
 
+    /**
+     * use it to add element to list
+     * @param value - the value that will be added
+     */
     public void push(int value) {
         if (last == null) last = new Node(value);
         else {
@@ -14,21 +18,38 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     *
+     * @return true when list is empty
+     */
     public boolean isEmpty() {
 
         return last == null;
     }
 
+    /**
+     *
+     * @return always false beacuse this implemention has no limit
+     */
     public boolean isFull() {
 
         return false;
     }
 
+    /**
+     * you get get value from top of list
+     * @return value or -1 on error
+     */
     public int top() {
         if (isEmpty()) return -1;
         return last.getValue();
     }
 
+    /**
+     * delate last added element
+     * set prev as new top node
+     * @return value of last element
+     */
     public int pop() {
         if (isEmpty()) return -1;
         int ret = last.getValue();
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,29 +5,49 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
-	public static void main(String[] args) {
-		RhymersFactory factory = new DefaultRhymersFactory();
-		
-		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-				factory.getFIFORhymer(), factory.getHanoiRhymer()};
-		
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
-				rhymers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < 15; i++)
-			rhymers[3].countIn(rn.nextInt(20));
-		
-		for (int i = 0; i < rhymers.length; i++) {
-			while (!rhymers[i].isEmpty())
-				System.out.print(rhymers[i].countOut() + "  ");
-			System.out.println();
-		}
-		
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
-	}
-	
+    public static void main(String[] args) {
+        RhymersFactory factory = new DefaultRhymersFactory();
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
+        int numbers = 15;
+        DefaultCountingOutRhymer[] rhymers = generateDefaultCountingOutRhymers(factory);
+        fillRhymers(rhymers, numbers);
+
+        int bound = 20;
+        DefaultCountingOutRhymer rhymer = rhymers[3];
+        countInGivenRhymer(numbers, bound, rhymer);
+        printTable(rhymers);
+        raportRejected(rhymers);
+    }
+
+    private static DefaultCountingOutRhymer[] generateDefaultCountingOutRhymers(RhymersFactory factory) {
+        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(), factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        return rhymers;
+    }
+
+    private static void fillRhymers(DefaultCountingOutRhymer[] rhymers, int size) {
+        for (int i = 1; i < size; i++)
+            for (int j = 0; j < rhymers.length; j++)
+                rhymers[j].countIn(i);
+    }
+
+    private static void printTable(DefaultCountingOutRhymer[] rhymers) {
+        for (int i = 0; i < rhymers.length; i++) {
+            while (!rhymers[i].isEmpty()) System.out.print(rhymers[i].countOut() + "  ");
+            System.out.println();
+        }
+    }
+
+    private static void countInGivenRhymer(int numbers, int bound, DefaultCountingOutRhymer rhymer) {
+        java.util.Random random = new java.util.Random();
+        for (int i = 1; i < numbers; i++)
+            rhymer.countIn(random.nextInt(bound));
+    }
+
+    private static void raportRejected(DefaultCountingOutRhymer[] rhymers) {
+        System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
+    }
 }
