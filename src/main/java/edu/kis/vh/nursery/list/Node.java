7e01358877cdbd261c3c876c7c0b910dde663855
AddyDouble <addydoubleyou@gmail.com>
2022-05-09 15:25:13 +0200

    1.5 dokumentacja klasy

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -16,22 +16,42 @@ public class Node {
         value = i;
     }
 
+    /**
+     *
+     * @return node value
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     *
+     * @return reference to previous node
+     */
     public Node getPrev() {
         return prev;
     }
 
+    /**
+     *
+     * @param prev sets previous node to prev
+     */
     public void setPrev(Node prev) {
         this.prev = prev;
     }
 
+    /**
+     *
+     * @return reference to next element
+     */
     public Node getNext() {
         return next;
     }
 
+    /**
+     *
+     * @param next sets next node to next
+     */
     public void setNext(Node next) {
         this.next = next;
     }
