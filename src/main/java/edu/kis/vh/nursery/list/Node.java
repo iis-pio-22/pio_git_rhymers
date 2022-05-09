e8e99bbb145877fcb817c222e37371fffe0fb53c
AddyDouble <addydoubleyou@gmail.com>
2022-05-09 15:08:02 +0200

    1.5.6 Dokumentacja klasy Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,10 +1,17 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Class represents element in list
+ */
 public class Node {
 
     public final int value;
     public Node prev, next;
 
+    /**
+     *
+     * @param i sets the value of node to i
+     */
     public Node(int i) {
         value = i;
     }
