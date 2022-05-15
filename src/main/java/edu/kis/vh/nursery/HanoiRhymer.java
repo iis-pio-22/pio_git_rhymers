e12b2a6ea4df4860c9c57188839f0da43e02bbe8
GabaNie <235931@edu.p.lodz.pl>
2022-05-15 22:03:53 +0200

    1.5.6 dokumentacja

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,23 @@
 package edu.kis.vh.nursery;
 
+/**
+ * @author kowallus z modyfikacjami Gabrieli Niemyjskiej
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	int totalRejected = 0;
 
+	/**
+	 * @return wartości totalRejected
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * @param in potrzebny do sprawdzenia czy jest większy od ostatniego
+	 *           elementu w tablicy
+	 */
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
 			totalRejected++;
