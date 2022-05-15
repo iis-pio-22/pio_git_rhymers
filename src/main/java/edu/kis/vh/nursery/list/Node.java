560b3abaeeb0c3c4b09a4090226111916a459f8e
ImArchee <kacper.devek@gmail.com>
2022-05-15 18:17:22 +0200

    1.5 - Testy, diagramy, dokumentacje

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,31 +1,58 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ *
+ */
 public class Node {
 
 	private final int value;
 	private Node prev;
 	private Node next;
 
+	/**
+	 * Constructor assigning 'value' to 'i'.
+	 * @param i
+	 */
 	public Node(int i) {
 		value = i;
 	}
 
+	/**
+	 * Returns previous Node.
+	 * @return previous Node
+	 */
 	public Node getPrev() {
 		return prev;
 	}
 
+	/**
+	 * Sets the previous Node to the argument passed to the method.
+	 * @param prev
+	 */
 	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
 
+	/**
+	 * Gets the next Node.
+	 * @return the next Node.
+	 */
 	public Node getNext() {
 		return next;
 	}
 
+	/**
+	 * Sets the next Node to the argument passed to the method.
+	 * @param next
+	 */
 	public void setNext(Node next) {
 		this.next = next;
 	}
 
+	/**
+	 * Getter that returns 'value'.
+	 * @return value
+	 */
 	public int getValue() {
 		return value;
 	}
