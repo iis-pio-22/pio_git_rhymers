d178e684d4613da8b45e188626f95cdc039c7321
kudo <235995@edu.p.lodz.pl>
2022-06-14 23:53:02 +0200

    zadanie 1.5.6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,6 +1,13 @@
 package edu.kis.vh.nursery;
 
+/**
+ *
+ */
+
 public class DefaultCountingOutRhymer {
+	/**
+	 * @param in
+	 */
 
 	public static final int CAPACITY = 12;
 	public static final int INIT_INDEX = -1;
@@ -8,25 +15,39 @@ public class DefaultCountingOutRhymer {
 
 	public int total = INIT_INDEX;
 
+	/**
+	 * @return
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			NUMBERS[++total] = in;
 	}
 
+	/**
+	 * @return
+	 */
 	public boolean callCheck() {
 		return total == INIT_INDEX;
 	}
-
+	/**
+	 * @return
+	 */
 	public boolean isFull() {
 		return total == CAPACITY-1;
 	}
 
+	/**
+	 * @return
+	 */
 	protected int peekaboo() {
 		if (callCheck())
 			return INIT_INDEX;
 		return NUMBERS[total];
 	}
 
+	/**
+	 * @return
+	 */
 	public int countOut() {
 		if (callCheck())
 			return INIT_INDEX;
