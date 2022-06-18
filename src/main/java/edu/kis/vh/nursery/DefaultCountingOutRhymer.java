e2fcc1f08f2aeb5464ff2877b4c14f72a2077d77
kudo <235995@edu.p.lodz.pl>
2022-06-18 16:04:54 +0200

    Merge branch 'master' into Accessory_i_hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,6 +11,7 @@ public class DefaultCountingOutRhymer {
 
 	public static final int CAPACITY = 12;
 	public static final int INIT_INDEX = -1;
+	public static final int FULL = 11;
 	private final int[] NUMBERS = new int[CAPACITY];
 
 	private int total = INIT_INDEX;
@@ -37,7 +38,8 @@ public class DefaultCountingOutRhymer {
 	 * @return
 	 */
 	protected boolean isFull() {
-		return total == CAPACITY-1;
+
+		return total == FULL;
 	}
 
 	/**
