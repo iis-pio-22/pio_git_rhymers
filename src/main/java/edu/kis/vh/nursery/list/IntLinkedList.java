0fd423c0ffe098a225e3abe9941f76070d0409fa
kudo <235995@edu.p.lodz.pl>
2022-06-20 20:10:55 +0200

    poprawa accessory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -4,8 +4,8 @@ public class IntLinkedList {
 
 
 	public static final int EMPTY = -1;
-	 private Node last;
-	 private int i;
+	 public Node last;
+	 public int i;
 
 	public void push(int i) {
 		if (last == null)
